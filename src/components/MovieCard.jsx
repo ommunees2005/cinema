0d0bc8f0ea608320bf619ade4881f36a2aaa9d6c
ommunees <ommunees48@gmail.com>
2025-06-27 14:02:36 +0530

    Update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,10 @@ import React from 'react';
 
 const MovieCard = ({ movie, onClick }) => {
   const imageSrc = require(`../assets/${movie.poster}`);
+  <div className="movie-card">
+  {/* Card content */}
+</div>
+
 
   return (
     <div className="movie-card" onClick={() => onClick(movie)} style={{ cursor: 'pointer' }}>
